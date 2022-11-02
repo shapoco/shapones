d878444e58376c37e2cca6837e8015a410a83ae3
shapoco <40152459+shapoco@users.noreply.github.com>
2022-11-02 23:12:06 +0900

    change SPI to PIO

--- samples/pico_ws19804/ws19804.hpp
+++ samples/pico_ws19804/ws19804.hpp
@@ -2,10 +2,12 @@
 
 #include "stdint.h"
 #include "pico/stdlib.h"
-#include "hardware/spi.h"
+#include "hardware/pio.h"
 #include "hardware/gpio.h"
 #include "hardware/clocks.h"
 #include "hardware/dma.h"
+
+#include "ws19804.pio.h"
 
 #include <algorithm>
 
@@ -13,19 +15,20 @@ class WS19804 {
 public:
     static constexpr int WIDTH = 320;
     static constexpr int HEIGHT = 240;
-    static constexpr uint PIN_UNUSED = 255;
     static constexpr uint PIN_DC = 8;
     static constexpr uint PIN_CS = 9;
     static constexpr uint PIN_SCK = 10;
     static constexpr uint PIN_DIN = 11;
     static constexpr uint PIN_RST = 12;
     static constexpr uint PIN_BL = 13;
-
-    spi_inst_t * const spi;
+    
+    PIO spi_pio;
+    uint spi_sm;
+    uint spi_offset;
     uint dma_tx;
     
     // Parallel init
-    WS19804() : spi(spi1)
+    WS19804()
     {
         gpio_init(PIN_RST);
         gpio_init(PIN_DC);
@@ -38,16 +41,26 @@ public:
         gpio_put(PIN_RST, 1);
         gpio_put(PIN_CS, 1);
         gpio_put(PIN_BL, 0);
-        
-        spi_init(spi, 62.5 * MHZ);
-        gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
-        gpio_set_function(PIN_DIN, GPIO_FUNC_SPI);
-        
-        // https://forums.raspberrypi.com/viewtopic.php?t=311664&start=25
-        //hw_write_masked(&spi_get_hw(spi)->cr0, 1 << SPI_SSPCR0_SCR_LSB, SPI_SSPCR0_SCR_BITS);
     }
-
+    
     void init() {
+        spi_pio = pio0;
+        spi_sm = 0;
+        spi_offset = pio_add_program(spi_pio, &ws19804_program);
+        
+        pio_gpio_init(spi_pio, PIN_DIN);
+        pio_gpio_init(spi_pio, PIN_SCK);
+        pio_sm_set_consecutive_pindirs(spi_pio, spi_sm, PIN_DIN, 1, true);
+        pio_sm_set_consecutive_pindirs(spi_pio, spi_sm, PIN_SCK, 1, true);
+        pio_sm_config pio_cfg = ws19804_program_get_default_config(spi_offset);
+        sm_config_set_sideset_pins(&pio_cfg, PIN_SCK);
+        sm_config_set_out_pins(&pio_cfg, PIN_DIN, 1);
+        sm_config_set_fifo_join(&pio_cfg, PIO_FIFO_JOIN_TX);
+        sm_config_set_clkdiv(&pio_cfg, 2.0f); // 250MHz / 2 / 2 --> 62.5 MHz
+        sm_config_set_out_shift(&pio_cfg, false, true, 8);
+        pio_sm_init(spi_pio, spi_sm, spi_offset, &pio_cfg);
+        pio_sm_set_enabled(spi_pio, spi_sm, true);
+
         dma_tx = dma_claim_unused_channel(true);
         
         // hardware reset
@@ -166,44 +179,65 @@ public:
             };
             write_command(0x2b, data, sizeof(data));
         }
-        //write_command(0x2c, (const uint8_t*)line_buff, w * h * 2);
+        //write_command(0x2c, data, w * h * 3 / 2);
         {
-            dma_channel_config c = dma_channel_get_default_config(dma_tx);
-            channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
-            channel_config_set_dreq(&c, spi_get_dreq(spi1, true));
-            dma_channel_configure(dma_tx, &c,
-                                  &spi_get_hw(spi1)->dr, // write address
-                                  data, // read address
-                                  w * h * 3 / 2, // element count (each element is of size transfer_data_size)
-                                  false); // don't start yet
-            
             gpio_put(PIN_DC, 0);
             gpio_put(PIN_CS, 0);
             uint8_t cmd = 0x2c;
-            spi_write_blocking(spi, &cmd, 1);
+            write_blocking(&cmd, 1);
             gpio_put(PIN_DC, 1);
+            
+            {
+                dma_channel_config dma_cfg = dma_channel_get_default_config(dma_tx);
+                channel_config_set_transfer_data_size(&dma_cfg, DMA_SIZE_8);
+                channel_config_set_dreq(&dma_cfg, pio_get_dreq(spi_pio, spi_sm, true));
+                dma_channel_configure(dma_tx, &dma_cfg,
+                                      &spi_pio->txf[spi_sm], // write address
+                                      data, // read address
+                                      w * h * 3 / 2, // element count (each element is of size transfer_data_size)
+                                      false); // don't start yet
+            }
+            
             dma_start_channel_mask(1u << dma_tx);
         }
     }
     
     void finish_write_data() {
         dma_channel_wait_for_finish_blocking(dma_tx);
-        while (spi_is_busy(spi)) { }
+        pio_wait_idle();
         gpio_put(PIN_CS, 1);
     }
 
     void write_command(uint8_t cmd, const uint8_t *data, int len) {
         gpio_put(PIN_DC, 0); // command mode
         gpio_put(PIN_CS, 0);
-        spi_write_blocking(spi, &cmd, 1);
+        write_blocking(&cmd, 1);
         if (data) {
             gpio_put(PIN_DC, 1); // data mode
-            spi_write_blocking(spi, data, len);
+            write_blocking(data, len);
         }
         gpio_put(PIN_CS, 1);
     }
   
     void write_command(uint8_t cmd) {
         write_command(cmd, nullptr, 0);
+    }
+    
+    void write_blocking(const uint8_t *data, int len) {
+        for(int i = 0; i < len; i++) {
+            pio_push(data[i]);
+        }
+        pio_wait_idle();
+    }
+    
+    void pio_push(uint8_t data) {
+        while (pio_sm_is_tx_fifo_full(spi_pio, spi_sm)) { }
+        *(volatile uint8_t*)&spi_pio->txf[spi_sm] = data;
+    }
+
+    void pio_wait_idle() {
+        uint32_t stall_mask = 1u << (spi_sm + PIO_FDEBUG_TXSTALL_LSB);
+        spi_pio->fdebug = stall_mask;
+        while (!(spi_pio->fdebug & stall_mask)) { }
     }
 };
